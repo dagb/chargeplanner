0452024e5b7b0aa037abc9cf6227a22ff71d208c
Dag Blakstad <dag@git.retrade.webstep.no>
2015-08-22 15:50:12 +0200

    Calculates charging time. Only curent SOC as input

---
+++ chargeplanner.js
@@ -0,0 +1,17 @@
+
+function startCharge(currentSOC, chargeLimit, targetTime, power, unit) {
+	currentSOCkWh = 0;
+	targetSOCKwH = 0;
+	if (unit == "%") {
+		currentSOCkWh = 74 * (currentSOC/100);
+		targetSOCKwH = (chargeLimit/100) * 74;
+	} else {
+		currentSOCkWh = 74 * (currentSOC/406);
+		targetSOCKwH = 74 * (chargeLimit / 406);
+	}
+
+	remainingChargekWh = targetSOCKwH - currentSOCkWh;
+
+	hours = remainingChargekWh / (power * 0.9);
+	return hours;
+}
