d09072cb7a72a578793910cd319eb50ca53ec9c8
Dag Blakstad <dag@git.retrade.webstep.no>
2015-08-29 11:58:34 +0200

    Better structure for handling different batteries

--- chargeplanner.js
+++ chargeplanner.js
@@ -1,14 +1,20 @@
 
+var batteryInfo = {};
+batteryInfo["60"] = { tkm: "250", "available" : (55) - (55) * 0.0625};
+batteryInfo["70"] = { tkm: "320", "available" : (65) - (65) * 0.0625};
+batteryInfo["85"]	= { tkm: "410", "available" : (80) - (80) * 0.0625};
+batteryInfo["90"]	= { tkm: "460", "available" : (85) - (85) * 0.0625};
+
 function chargeTime(currentSOC, chargeLimit, power, unit, battery) {
 	currentSOCkWh = 0;
 	targetSOCKwH = 0;
-	batteryAvailable = (battery - 5) - (battery - 5) * 0.0625;
+	var batteryData = batteryInfo[battery];
 	if (unit == "%") {
-		currentSOCkWh = batteryAvailable * (currentSOC/100);
-		targetSOCKwH = (chargeLimit/100) * batteryAvailable;
+		currentSOCkWh = batteryData.available * (currentSOC/100);
+		targetSOCKwH = (chargeLimit/100) * batteryData.available;
 	} else {
-		currentSOCkWh = batteryAvailable * (currentSOC/406);
-		targetSOCKwH = batteryAvailable * (chargeLimit / 406);
+		currentSOCkWh = batteryData.available * (currentSOC/batteryData.tkm);
+		targetSOCKwH = batteryData.available * (chargeLimit / batteryData.tkm);
 	}
 
 	remainingChargekWh = targetSOCKwH - currentSOCkWh;
